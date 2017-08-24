eb1e081e2953fa93ca37e9165dc947db76062faa
Joseph Sungpil Choi <chousemath@gmail.com>
2017-08-24 20:10:30 +0900

    testing new deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,31 +3,29 @@
 GREEN='\033[0;32m'
 LIGHTCYAN='\033[1;36m'
 NC='\033[0m'
-
-# echo -e "${GREEN}Type your commit comment, followed by [ENTER]:${NC}"
-# # the read command stores the user input as a string
-# read comment
-# # the -e command enables the backslash necessary for colored text
-# echo -e "${GREEN}commit comment has been loaded${NC}"
-# # just a spacer
-# echo -e "${LIGHTCYAN}............${NC}"
-#
-# echo -e "${GREEN}git add . started${NC}"
-# git add .
-# echo -e "${GREEN}git add . completed${NC}"
-# echo -e "${LIGHTCYAN}............${NC}"
-# echo -e "${GREEN}git commit -am started${NC}"
-# eval "git commit -am \"${comment}\""
-# echo -e "${GREEN}git commit -am completed${NC}"
-# echo -e "${LIGHTCYAN}............${NC}"
-# echo -e "${GREEN}git push started${NC}"
-# git push
-# echo -e "${GREEN}git push completed${NC}"
-#
-# echo -e "${GREEN}npm run deploy started${NC}"
-# npm run deploy
-# echo -e "${GREEN}npm run deploy completed${NC}"
-# echo -e "${LIGHTCYAN}............${NC}"
-#
-# echo -e "${LIGHTCYAN}............${NC}"
-# echo -e "${GREEN}deployment complete!${NC}"
+echo -e "${GREEN}Type your commit comment, followed by [ENTER]:${NC}"
+# the read command stores the user input as a string
+read comment
+# the -e command enables the backslash necessary for colored text
+echo -e "${GREEN}commit comment has been loaded${NC}"
+# just a spacer
+echo -e "${LIGHTCYAN}............${NC}"
+echo -e "${GREEN}converting jupyter notebooks to html${NC}"
+cd posts/1 && jupyter nbconvert index.ipynb
+cd ../..
+echo -e "${GREEN}conversion of jupyter notebooks complete!${NC}"
+# just a spacer
+echo -e "${LIGHTCYAN}............${NC}"
+echo -e "${GREEN}git add . started${NC}"
+git add .
+echo -e "${GREEN}git add . completed${NC}"
+echo -e "${LIGHTCYAN}............${NC}"
+echo -e "${GREEN}git commit -am started${NC}"
+eval "git commit -am \"${comment}\""
+echo -e "${GREEN}git commit -am completed${NC}"
+echo -e "${LIGHTCYAN}............${NC}"
+echo -e "${GREEN}git push started${NC}"
+git push
+echo -e "${GREEN}git push completed${NC}"
+echo -e "${LIGHTCYAN}............${NC}"
+echo -e "${GREEN}deployment complete!${NC}"
